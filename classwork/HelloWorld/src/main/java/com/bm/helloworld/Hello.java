00e6f6fdb12723960a9dce19ef3608b7b3ead6b6
Benjamin Munoz <driver396@gmail.com>
2021-07-26 11:19:28 -0700

    Code-Along: NetBeans Hello World!

---
+++ classwork/HelloWorld/src/main/java/com/bm/helloworld/Hello.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Benjamin Munoz
+ * email: driver396@gmail.com
+ * date: July 26, 2021
+ * purpose: First Hello World program in the NetBeans IDE
+ */
+package com.bm.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Benjamin!");
+    }
+}
