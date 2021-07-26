5a38fe58ff46dda1189bddb3f6d402c17c7bfa77
Benjamin Munoz <driver396@gmail.com>
2021-07-26 10:49:05 -0700

    Practice Activity for first Code-along

--- code_alongs/Hello.java
+++ code_alongs/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("My name is Benjamin Munoz");
+        System.out.println("I currently live in Issaquah, WA");
+        System.out.println("which is roughly a 30-minute drive from");
+        System.out.println("Seattle, WA");
     }
 }
